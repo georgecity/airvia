e300179df3f6e535c3c35f5880798ab7f9eef49c
georgecity <57677719+georgecity@users.noreply.github.com>
2020-06-11 19:25:04 +0100

    initial

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const users = require('./backend/routes/api/users');
 const app = express();
 const path = require('path');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
